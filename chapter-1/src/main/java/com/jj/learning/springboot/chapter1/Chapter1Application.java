76ede25e4295071c996f7b292f7b64d2ce51c4f8
morosedog <jj.huang@uni-net.com.tw>
2019-03-13 14:14:01 +0800

    chapter-1

---
+++ chapter-1/src/main/java/com/jj/learning/springboot/chapter1/Chapter1Application.java
@@ -0,0 +1,13 @@
+package com.jj.learning.springboot.chapter1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Chapter1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Chapter1Application.class, args);
+	}
+
+}
---
+++ chapter-1/src/main/java/com/jj/learning/springboot/chapter1/controller/DemoController.java
@@ -0,0 +1,15 @@
+package com.jj.learning.springboot.chapter1.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class DemoController {
+    
+    @RequestMapping(value = "/demo", method = RequestMethod.GET)
+    public String demo(){
+        return "Hello World!";
+    }
+
+}
