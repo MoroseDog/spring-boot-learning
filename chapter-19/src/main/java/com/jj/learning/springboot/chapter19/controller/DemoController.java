86525b8af4b46b54cae662bf0388cf5db241cbed
Jyunjia Huang <moroseadog@gmail.com>
2019-04-04 21:12:11 +0800

    chapter-19 update

--- chapter-19/src/main/java/com/jj/learning/springboot/chapter19/controller/DemoController.java
+++ chapter-19/src/main/java/com/jj/learning/springboot/chapter19/controller/DemoController.java
@@ -1,7 +1,11 @@
 package com.jj.learning.springboot.chapter19.controller;
 
+import java.util.List;
+
 import javax.validation.Valid;
 
+import org.springframework.validation.BindingResult;
+import org.springframework.validation.ObjectError;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
@@ -13,6 +17,22 @@ public class DemoController {
 
     @RequestMapping(value = "/demo", method = RequestMethod.GET)
     public String demo(@Valid Customer customer) {
+        return "姓名：" + customer.getName() + ", 年齡：" + customer.getAge() + ", 創建者：" + customer.getCreateBy();
+    }
+
+    @RequestMapping(value = "/demo/bindingResult", method = RequestMethod.GET)
+    public String demo(@Valid Customer customer, BindingResult bindingResult) {
+        StringBuilder sb = new StringBuilder();
+        if (bindingResult.hasErrors()) {
+            List<ObjectError> errorList = bindingResult.getAllErrors();
+            for (ObjectError error : errorList) {
+                System.out.println(error.getDefaultMessage());
+                sb.append(error.getDefaultMessage());
+            }
+            if (sb.length() > 0) {
+                return sb.toString();
+            }
+        }
         return "姓名：" + customer.getName() + ", 年齡：" + customer.getAge() + ", 創建者：" + customer.getCreateBy();
     }
 
