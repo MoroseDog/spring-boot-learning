fa3e8132cfe11830800380d22d5e1a5af58026c4
Jyunjia Huang <moroseadog@gmail.com>
2019-05-07 12:09:50 +0800

    chapter-42

---
+++ chapter-42/src/main/java/com/jj/learning/springboot/chapter42/Chapter42Application.java
@@ -0,0 +1,13 @@
+package com.jj.learning.springboot.chapter42;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Chapter42Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Chapter42Application.class, args);
+	}
+
+}
